942e2b4fc79f090101d8636a0d73b136b78cde9c
Akarsh Khandelwal <115661993+akarshkh@users.noreply.github.com>
2024-12-01 15:19:07 +0530

    Update CardPopularProducts.tsx

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -25,7 +25,7 @@ const CardPopularProducts = () => {
               >
                 <div className="flex items-center gap-3">
                   <Image
-                    src={`https://zenvpc.s3.ap-south-1.amazonaws.com/logo.png/product${
+                    src={`https://zenvpc.s3.ap-south-1.amazonaws.com/product${
                       Math.floor(Math.random() * 3) + 1
                     }.png`}
                     alt={product.name}
